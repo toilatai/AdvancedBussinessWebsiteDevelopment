ed2300510cee7f2b54c2ffdaad6da3c57a1a5a10
toilatai <luongtantai51@gmail.com>
2026-01-20 07:56:04 +0700

    Add files via upload

---
+++ Ex18/group-customers/group-customers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-customers',
+  imports: [],
+  templateUrl: './group-customers.html',
+  styleUrl: './group-customers.css',
+})
+export class GroupCustomers {
+
+}
